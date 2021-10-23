3951dc8db47e329d3d867dbd0573fad1a32a5a84
prajith1982 <prajith1982@gmail.com>
2021-10-23 18:41:50 +0530

    Spring and build system

---
+++ src/main/kotlin/com/vot/ProjectManagerLte/ProjectManagerLteApplication.kt
@@ -0,0 +1,11 @@
+package com.vot.ProjectManagerLte
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectManagerLteApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectManagerLteApplication>(*args)
+}
---
+++ src/test/kotlin/com/vot/ProjectManagerLte/ProjectManagerLteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vot.ProjectManagerLte
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjectManagerLteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
